63fb6a32181f929a443a06851c3de7a55ec22a07
Anastasia Radaeva <radas2001@mail.ru>
2021-02-20 02:50:42 +0300

    add programm

---
+++ Test/Test.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+char line()
+{
+	char s;
+	char total[255];
+	int i = 0;
+
+	printf("Write your line (write line with # at the end to exit): \n");
+
+	while ((s = getchar()) != '#') {
+		if (s == ' ' || s == '\t')
+			++i;
+		else if (s != ' ' || s != '\t') {
+			if (i != 0) 
+				printf("  ");
+			putchar(s);
+			i = 0;
+		}
+
+	}
+	while (getchar() != '#') {
+		total[i] = getchar();
+		i++;
+	}
+	return total[255];
+}
+
+int main()
+{
+	line();
+}
+
+
