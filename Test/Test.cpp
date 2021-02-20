35a55fe1aa694dd9bebe91394c4d31122208725c
Anastasia Radaeva <radas2001@mail.ru>
2021-02-20 13:52:53 +0300

    fix program

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -3,27 +3,21 @@
 char line()
 {
 	char s;
-	char total[255];
 	int i = 0;
 
-	printf("Write your line (write line with # at the end to exit): \n");
+	printf("Write your line (write # at the end to exit): \n");
 
 	while ((s = getchar()) != '#') {
 		if (s == ' ' || s == '\t')
 			++i;
 		else if (s != ' ' || s != '\t') {
-			if (i != 0) 
-				printf("  ");
-			putchar(s);
-			i = 0;
+				if (i != 0) 
+					printf("  "); 
+				putchar(s);
+				i = 0;
 		}
-
 	}
-	while (getchar() != '#') {
-		total[i] = getchar();
-		i++;
-	}
-	return total[255];
+	return 0;
 }
 
 int main()
